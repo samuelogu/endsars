c2ddbae5a7935cc75cc51945c4ad80dd320b3d9d
Samuel Ogu <hello@samuelogu.com>
2020-10-10 16:14:37 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const express = require("express");
 const dotenv = require('dotenv');
 const tweets = require('./tweets')
 
-const mainHashtag = "#EndSARS"
-const hashtags = require('./hashtags')
 
     dotenv.config();
 
@@ -19,6 +17,9 @@ const twitter = new Twitter({
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
+
+const mainHashtag = "#EndSARS"
+const hashtags = require('./hashtags')
 
 // schedule tweet to been sent from the server every minute
 cron.schedule("* * * * *", async function() {
