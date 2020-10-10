a84e4318711cf245e56fcf1faaa915aeed56315b
Samuel Ogu <hello@samuelogu.com>
2020-10-10 15:32:54 +0100

    seperated the tweets to a file

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const cron = require("node-cron");
 const express = require("express");
 const dotenv = require('dotenv');
+const tweets = require('./tweets')
 dotenv.config();
 
 app = express()
@@ -14,30 +15,15 @@ const twitter = new Twitter({
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-const tweets = [
-    "I'm not a criminal if I use an Iphone",
-    "I hate police brutality",
-    "This is long overdue",
-    "Enough is enough",
-    "Bitcoin is not a crime",
-    "Being a web developer is not a crime",
-    "Being a graphics designer is not a crime",
-    "Being young and successful is not a crime",
-    "Being a forex trader is not a crime",
-    "Ifeoma Abugu was raped and murdered while in detention #RIP",
-    "Chibuike Anams was killed while sitting with his friends in a guest house #RIP",
-    "Aneka Okorie was killed because he refused to pay bribe ‪🤦🏽‍ #RIP",
-    "Christian Ugwuoke was killed while attending a wake keep for his aunt #RIP",
-    "It could be you",
-    "No reforming!!! Just end it!!!"
-]
-
 const hastags = "#EndSARS #EndSarsNow #EndPoliceBrutality #EndSARSBrutality"
 
+/*const tweet = tweets[Math.floor(Math.random() * tweets.length)] + " "+hastags
+console.log(tweets);
+return false;*/
+
 // schedule tweet to been sent from the server every minute
-cron.schedule("* * * * *", async function() {
+cron.schedule("*/5 * * * *", async function() {
     const tweet = tweets[Math.floor(Math.random() * tweets.length)] + " "+hastags
-    // console.log(tweet);
     twitter.post('statuses/update', {status: tweet}, (error, tweet, response) => {
 
     });
---
+++ tweets.js
@@ -0,0 +1,17 @@
+module.exports = [
+    "I'm not a criminal if I use an Iphone",
+    "I hate police brutality",
+    "This is long overdue",
+    "Enough is enough",
+    "Bitcoin is not a crime",
+    "Being a web developer is not a crime",
+    "Being a graphics designer is not a crime",
+    "Being young and successful is not a crime",
+    "Being a forex trader is not a crime",
+    "Ifeoma Abugu was raped and murdered while in detention #RIP",
+    "Chibuike Anams was killed while sitting with his friends in a guest house #RIP",
+    "Aneka Okorie was killed because he refused to pay bribe ‪🤦🏽‍ #RIP",
+    "Christian Ugwuoke was killed while attending a wake keep for his aunt #RIP",
+    "It could be you",
+    "No reforming!!! Just end it!!!"
+]
